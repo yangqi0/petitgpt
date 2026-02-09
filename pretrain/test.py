26265e4cae6b0a9b5c4919d964b4c5350e3fd8db
Yang Qi <qiyang0730@gmail.com>
2026-02-09 16:05:20 +0000

    init sft

--- pretrain/test.py
+++ pretrain/test.py
@@ -1,17 +1,6 @@
 import numpy as np
 from pathlib import Path
 
-bos_id = 2
-eos_id = 3
+import torch
 
-f = sorted(Path("datasets/pretrain_mix/train").glob("*.bin"))[0]
-x = np.memmap(f, dtype=np.uint16, mode="r")
-
-prev = x[:-1]
-nxt = x[1:]
-
-bos_cnt = int((prev == bos_id).sum())
-bos_to_eos = int(((prev == bos_id) & (nxt == eos_id)).sum())
-
-print("bos_cnt:", bos_cnt)
-print("bos->eos:", bos_to_eos, "ratio:", (bos_to_eos / max(1, bos_cnt)))
+print(torch.load("../outputs/pretrain_120m/latest.pt").keys())
